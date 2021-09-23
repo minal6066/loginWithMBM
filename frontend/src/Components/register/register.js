57da9b52365edf14eea6df0e9dfc143155fa2167
Puru soni <purusoni220@gmail.com>
2021-09-23 23:12:20 +0530

    page updation

--- frontend/src/Components/register/register.js
+++ frontend/src/Components/register/register.js
@@ -9,23 +9,25 @@ class Register extends Component {
 
     render() {
         return (
-            <div id="container">
-                <div id="left">
-                    <h1 id="head">Register</h1>
-                    <div id="logo"></div>
-                    <p id="para">To keep connected with us register here with your personal info.</p>
-                </div>
-                <div id="right">
-                    <h1 id="head-2">Enter your details</h1>
-                    <div id="details">
-                        <input type="text" placeholder="Enter your name" name="name" id="item" />
-                        <input type="text" placeholder="Enter your roll no" name="roll_no" id="item" />
-                        <input type="email" name="email" placeholder="Enter your email" id="item" />
-                        <input type="number" name="phone" placeholder="Enter your Phone no" id="item" />
-                        <input type="password" placeholder="Enter your password" name="Password" id="item" />
-                        <input type="password" placeholder="Confirm your password" name="Password" id="item" />
+            <div className="container">
+                <div className="left">
+                    <h1 className="head">Register</h1>
+                    <div className="logo">
+
                     </div>
-                    <input type="button" value="Register" id="button" />
+                    <h3 className="para">To keep connected with us register here with your personal info.</h3>
+                </div>
+                <div className="right">
+                    <h1 className="head-2">Enter your details</h1>
+                    <div className="details">
+                        <input type="text" placeholder="Enter your name" name="name" className="item" />
+                        <input type="text" placeholder="Enter your roll no" name="roll_no" className="item" />
+                        <input type="email" name="email" placeholder="Enter your email" className="item" />
+                        <input type="number" name="phone" placeholder="Enter your Phone no" className="item" />
+                        <input type="password" placeholder="Enter your password" name="Password" className="item" />
+                        <input type="password" placeholder="Confirm your password" name="Password" className="item" />
+                    </div>
+                    <input type="button" value="Register" className="button" />
                 </div>
             </div>
         )
